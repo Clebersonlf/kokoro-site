372f60bc2d8f156d13b117982aa4ecb51a60783c
Clebersonlf <clebersonlf@gmail.com>
2026-02-05 08:53:51 -0300

    Home redirect to /admin.html (real page)

--- app/page.js
+++ app/page.js
@@ -1,12 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <html>
-      <head>
-        <meta http-equiv="refresh" content="0; url=/user/cadastro/cadastro.html" />
-      </head>
-      <body>
-        <p>Redirecionando...</p>
-      </body>
-    </html>
-  );
+  redirect('/admin.html');
 }
