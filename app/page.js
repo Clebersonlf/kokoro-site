71a0fa68e1a136440a443e2fbffc6d2f4529f5da
Clebersonlf <clebersonlf@gmail.com>
2026-02-08 15:10:13 -0300

    Restore site state to ba9387a

--- app/page.js
+++ app/page.js
@@ -1,11 +1,5 @@
+import { redirect } from 'next/navigation';
+
 export default function Home() {
-  return (
-    <main style={{ margin: 0, padding: 0, width: '100vw', height: '100vh' }}>
-      <iframe
-        src="/home.html"
-        style={{ border: 'none', width: '100%', height: '100%' }}
-        title="KOKORO"
-      />
-    </main>
-  );
+  redirect('/admin.html');
 }
